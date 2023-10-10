65691fa1129b45cd76f2ef751314b1eee60fb2b7
twilson24 <146988075+twilson24@users.noreply.github.com>
2023-10-10 15:16:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
-
+ 
 
 
